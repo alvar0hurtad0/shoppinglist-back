d1a95273bfba73633964df0b98e36d369da626b3
Alvaro J. Hurtado <alvaroh@aspgems.com>
2016-06-06 11:52:40 +0200

    Initial commit.

---
+++ rebuild.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+chown drupaluser:www-data /var/www/html/ -R
+chown www-data:www-data /var/www/html/sites/default -R
+drush config-set system.site uuid medea-website
+/etc/init.d/ssh start
+apachectl start
+drush cim
+drush cr
+apachectl restart
+tail -f /var/log/apache2/error.log
